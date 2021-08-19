7b58b325cdd26385f5b827ca9784193584f99fd2
aggreyah <aggreyah@gmail.com>
2021-08-19 20:01:42 +0300

    some string formatting lesson

--- Basics/string_formatting.py
+++ Basics/string_formatting.py
@@ -3,5 +3,6 @@ last_name = "Ochieng"
 
 # output = "Hello, " + first_name + " " + last_name
 # output = "Hello, {} {}".format(first_name, last_name)
-output = "Hello, {1} {0}".format(first_name, last_name)
+# output = "Hello, {1} {0}".format(first_name, last_name)
+output = f"Hello, {first_name} {last_name}"
 print(output)
