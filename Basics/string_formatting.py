23d6f5224c483f991339eb0754b56f7b498fc9cb
aggreyah <aggreyah@gmail.com>
2021-08-19 20:00:22 +0300

    some string formatting lesson

---
+++ Basics/string_formatting.py
@@ -0,0 +1,7 @@
+first_name = "Aggrey"
+last_name = "Ochieng"
+
+# output = "Hello, " + first_name + " " + last_name
+# output = "Hello, {} {}".format(first_name, last_name)
+output = "Hello, {1} {0}".format(first_name, last_name)
+print(output)
